ee8bf0f75cfd5df8b531000193f4a4fc0e8d59c7
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-13 23:17:58 +0630

    fetch data from firebase

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,42 +1,31 @@
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Post from './Post'
-const DUMMY_DATA = [
-    {
-        id: '123',
-        username: 'kaungmyathan',
-        userImg:
-            'https://scontent.fmdl4-3.fna.fbcdn.net/v/t1.6435-9/33083052_375460019611960_2067366586485833728_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-803kPWisC0AX_erz7y&_nc_ht=scontent.fmdl4-3.fna&oh=00_AT_Ni8q5gUXxREyku6ZKm21nuSQSeXhnvZnvYUDJGRcZrw&oe=622E281B',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'This is DOPE!',
-    },
-    {
-        id: '124',
-        username: 'kaungmyathan',
-        userImg:
-            'https://scontent.fmdl4-3.fna.fbcdn.net/v/t1.6435-9/33083052_375460019611960_2067366586485833728_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-803kPWisC0AX_erz7y&_nc_ht=scontent.fmdl4-3.fna&oh=00_AT_Ni8q5gUXxREyku6ZKm21nuSQSeXhnvZnvYUDJGRcZrw&oe=622E281B',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'This is DOPE!',
-    },
-    {
-        id: '125',
-        username: 'kaungmyathan',
-        userImg:
-            'https://scontent.fmdl4-3.fna.fbcdn.net/v/t1.6435-9/33083052_375460019611960_2067366586485833728_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=-803kPWisC0AX_erz7y&_nc_ht=scontent.fmdl4-3.fna&oh=00_AT_Ni8q5gUXxREyku6ZKm21nuSQSeXhnvZnvYUDJGRcZrw&oe=622E281B',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'This is DOPE!',
-    },
-]
+
 function Posts () {
+    const [posts, setPosts] = useState<any>([])
+    useEffect(
+        () =>
+            onSnapshot(
+                query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+                (snapshot) => {
+                    setPosts(snapshot.docs)
+                }
+            ),
+        [db]
+    )
+
     return (
         <div>
-            {DUMMY_DATA.map((post) => (
+            {posts.map((post: any) => (
                 <Post
                     key={post.id}
                     id={post.id}
-                    username={post.username}
-                    userImg={post.userImg}
-                    img={post.img}
-                    caption={post.caption}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
                 />
             ))}
         </div>
