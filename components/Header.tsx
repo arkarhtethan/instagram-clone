200d71f88a8b5c26f8726f634e0fd6b7b7cd47f8
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-13 18:34:07 +0630

    recoil added

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,16 +11,18 @@ import {
 import { HomeIcon } from '@heroicons/react/solid'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
+import { modalState } from '../atoms/modalAtom'
+import { useRecoilState } from 'recoil'
 
 function Header () {
-    const { data: session } = useSession();
+    const { data: session } = useSession()
+    const [open, setOpen] = useRecoilState(modalState)
     return (
-        <div className="shadow-sm border-b bg-white sticky top-0 z-10">
+        <div className="sticky top-0 z-10 border-b bg-white shadow-sm">
             <div className="mx-5 flex max-w-6xl justify-between lg:mx-auto">
                 {/* Left */}
                 <Link href="/">
                     <div className="relative hidden w-24 cursor-pointer lg:inline-grid">
-
                         <Image
                             src="https://links.papareact.com/ocw"
                             layout="fill"
@@ -55,25 +57,31 @@ function Header () {
                 <div className="flex items-center justify-end space-x-4">
                     <HomeIcon className="navBtn" />
                     <MenuIcon className="h-6 w-10 cursor-pointer md:hidden" />
-                    {session ? (<>     <div className="navBtn relative">
-                        <PaperAirplaneIcon className="navBtn rotate-45" />
-                        <div className="absolute -top-1 -right-2 text-xs h-5 w-5 flex items-center justify-center rounded-full bg-red-500 animate-pulse text-white">
-                            3
-                        </div>
-                    </div>
-                        <PlusCircleIcon className="navBtn" />
-                        <UserGroupIcon className="navBtn" />
-                        <HeartIcon className="navBtn" />
-
-                        <img
-                            onClick={signOut}
-                            src={session?.user?.image || ""}
-                            alt="profile"
-                            className="h-10 cursor-pointer rounded-full"
-                        /></>) : (
+                    {session ? (
+                        <>
+                            {' '}
+                            <div className="navBtn relative">
+                                <PaperAirplaneIcon className="navBtn rotate-45" />
+                                <div className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                                    3
+                                </div>
+                            </div>
+                            <PlusCircleIcon
+                                onClick={() => setOpen(true)}
+                                className="navBtn"
+                            />
+                            <UserGroupIcon className="navBtn" />
+                            <HeartIcon className="navBtn" />
+                            <img
+                                onClick={signOut}
+                                src={session?.user?.image || ''}
+                                alt="profile"
+                                className="h-10 cursor-pointer rounded-full"
+                            />
+                        </>
+                    ) : (
                         <button onClick={signIn}>signin</button>
                     )}
-
                 </div>
             </div>
         </div>
