bd7567b543fda34c19a72b8911b21f456a0f1d49
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-13 18:28:02 +0630

    container style added and feed component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 export default function Home () {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       {/* Header */}
       <Header />
       {/* Feed */}
+      <Feed />
       {/* Modal */}
     </div>
   )
