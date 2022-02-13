00c1fb0839fd0bacabe8c5edb64c37fb3348aa79
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-13 23:17:38 +0630

    remove clg line

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -13,7 +13,6 @@ function Stories () {
         }))
         setSuggestiongs(suggestions)
     }, [])
-    console.log(faker.internet.avatar());
     return (
         <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
             {session && <Story
