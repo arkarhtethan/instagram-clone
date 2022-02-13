e594a68a741fef312eeb4787e3cfa46d68d2408f
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-13 23:20:05 +0630

    seo tag added

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,12 @@ export default function Home () {
 
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+      <Head>
+        <title>Kmgram | Home</title>
+        <meta name="description" content="Instagram clone build for educational purpuose" />
+        <meta name="keywords" content="Instagram React clone" />
+        <meta name="author" content="Kaung Myat Han" />
+      </Head>
       {/* Header */}
       <Header />
       {/* Feed */}
